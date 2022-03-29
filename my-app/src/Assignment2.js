3512e2a31d9493fe18fa7f70e83641445778adad
palakchhina <chhinapalak2@gmail.com>
2022-03-29 22:51:51 +0530

    assignment 2 file has been updated

--- my-app/src/Assignment2.js
+++ my-app/src/Assignment2.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const assignment2 =() =>{
     return(
+      <div>
+        
         <h1>Assignment 2</h1>
+        <h3>code has been uploaded on git</h3>
+      </div>
+        
         );
       }
         
